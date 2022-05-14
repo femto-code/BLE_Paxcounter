9cec972701151562582fc0ce2758163ed57a1b79
femto-code <59444046+femto-code@users.noreply.github.com>
2022-05-14 22:17:47 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,35 @@
 #include "BLEAdvertisedDevice.h"
 #include "sdkconfig.h"
 
+#include <ArduinoJson.h>
+
+#define TX 1
+#define RX 3
+
+// Create the JSON document
+StaticJsonDocument<250> doc;
+char buffer[250];
+
+//HardwareSerial &hSerial = Serial1; // can be Serial2 as well, just use proper pins
 
 static const char LOG_TAG[] = "SampleScan";
 
-void setup() {}
+void setup() {
+	Serial.begin(115200);  // open serial via USB to PC on default port
+	//hSerial.begin(115200, SERIAL_8N1, RX, TX); // open the other serial port
+}
 
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
-	void onResult(BLEAdvertisedDevice advertisedDevice) {
+class MyAdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+	void onResult(BLEAdvertisedDevice advertisedDevice)
+	{
 
 		std::string res = "";
-		char *pHex = BLEUtils::buildHexData(nullptr, (uint8_t*)advertisedDevice.getManufacturerData().data(), advertisedDevice.getManufacturerData().length());
+		char *pHex = BLEUtils::buildHexData(nullptr, (uint8_t *)advertisedDevice.getManufacturerData().data(), advertisedDevice.getManufacturerData().length());
 		res += pHex;
-		
-		if (res.find("bc040a") != std::string::npos) {
+		// ESP_LOGE(LOG_TAG, "res: %s", res.c_str());
+		if (res.find("bc040a") != std::string::npos)
+		{
 			ESP_LOGE(LOG_TAG, "PAC found! Address: %s", advertisedDevice.getAddress().toString().c_str());
 			ESP_LOGE(LOG_TAG, "Byte_count: %d", advertisedDevice.getManufacturerData().length());
 			
@@ -82,38 +98,64 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
 			ESP_LOGE(LOG_TAG, "serial_nmbr upper3 decoded: %s", serial_nmbr_decoded.data());
 
 			ESP_LOGE(LOG_TAG, "device_status: %s", device_status.data());
+
+			// JsonObject obj = doc.createNestedObject();
+			// obj["mac"] = advertisedDevice.getAddress().toString().c_str();
+			// obj["pn"] = part_nmbr_decoded;
+			// obj["ds"] = device_status;
+			// obj["sn"] = serial_nmbr_decoded;
+			// serializeJson(doc, buffer);
+			JsonObject obj = doc.createNestedObject();
+			obj["mac"] = 1;
+			obj["pn"] = 2;
+			obj["ds"] = 3;
+			obj["sn"] = 4;
+			serializeJson(doc, buffer);
+			
 		}
-		return;
 
 
 		// ESP_LOGE(LOG_TAG, "payload: %x", advertisedDevice.getPayload());
-		ESP_LOGE(LOG_TAG, "man_data: %x", advertisedDevice.getManufacturerData().data());
-		std::string temp = advertisedDevice.getManufacturerData();
-		ESP_LOGE(LOG_TAG, "temp: %d", temp.c_str());
-		if (temp.find("bc04") != std::string::npos) {
-			ESP_LOGE(LOG_TAG, "Dräger found!");
+		// ESP_LOGE(LOG_TAG, "man_data: %x", advertisedDevice.getManufacturerData().data());
+		// std::string temp = advertisedDevice.getManufacturerData();
+		// ESP_LOGE(LOG_TAG, "temp: %d", temp.c_str());
+		// if (temp.find("bc04") != std::string::npos) {
+		// 	ESP_LOGE(LOG_TAG, "Dräger found!");
 			
 			
-		}
-		ESP_LOGE(LOG_TAG, "address: %s", advertisedDevice.getAddress().toString().c_str());
-		if (advertisedDevice.haveName()) {
-			ESP_LOGE(LOG_TAG, "Advertised Device: %s", advertisedDevice.toString().c_str());
-		}
+		// }
+		// ESP_LOGE(LOG_TAG, "address: %s", advertisedDevice.getAddress().toString().c_str());
+		// if (advertisedDevice.haveName()) {
+		// 	ESP_LOGE(LOG_TAG, "Advertised Device: %s", advertisedDevice.toString().c_str());
+		// }
 	}
 };
 
 static void run() {
 	ESP_LOGE(LOG_TAG, "Scanning sample starting");
 	BLEDevice::init("");
-	BLEScan* pBLEScan = BLEDevice::getScan();
+	BLEScan *pBLEScan = BLEDevice::getScan();
 	pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
 	pBLEScan->setActiveScan(true);
 	BLEScanResults scanResults = pBLEScan->start(30);
 	ESP_LOGE(LOG_TAG, "We found %d devices", scanResults.getCount());
 	ESP_LOGE(LOG_TAG, "Scanning sample ended");
+	Serial.write(buffer);
+	ESP_LOGE(LOG_TAG, "Buffer: %s", buffer);
 }
 
-void loop(void)
-{
+void loop(void) {
 	run();
+	// if (Serial.available()) { // check incoming on default serial (USB) from PC
+	// 	hSerial.write("From Serial:" + Serial.read()); // read it from UBS and send it to hSerial
+	// }
+
+	// if (hSerial.available()) { // check incoming on other serial from the other board
+	// 	Serial.write("From hSerial:" + hSerial.read()); // read it from hSerial and send it to  UBS
+	// }
+	//hSerial.write("1");
+	Serial.write("2");
+	//hSerial.print(buffer);
+	Serial.print(buffer);
+	delay(3000);
 } // app_main
